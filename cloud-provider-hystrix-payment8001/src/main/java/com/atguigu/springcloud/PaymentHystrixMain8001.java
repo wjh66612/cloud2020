efba4c8a0ea764723835b54cf4e37918d685c375
wjh66612 <1529659488@qq.com>
2020-11-02 17:31:15 +0800

    [Add] 3次测试合并到master

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/PaymentHystrixMain8001.java
@@ -13,6 +13,5 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class,args);
-
     }
 }
